e74928a49b5548fcb932ba8c99651e22736612d1
Jon <33264023+jonettic@users.noreply.github.com>
2017-11-04 01:51:09 +0400

    added packages class strining packages together

---
+++ 04_packages/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jonettic/GoClass/04_packages/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+}
